a2901c423b0c8c564f592738fc1b0b604fde3a35
James (PD) <69664712+JamesDPC@users.noreply.github.com>
2025-12-09 17:11:45 +1100

    Handle ownership

--- src/TokenAccessible.php
+++ src/TokenAccessible.php
@@ -20,12 +20,15 @@ class TokenAccessible extends DataExtension
 
     public function onBeforeWrite()
     {
-        if (!$this->getOwner()->Token) {
-            $this->getOwner()->RegenerateTokens = true;
+
+        /** @var \SilverStripe\ORM\DataObject $owner */
+        $owner = $this->getOwner();
+        if (!$owner->Token) {
+            $owner->RegenerateTokens = true;
         }
 
-        if ($this->getOwner()->RegenerateTokens) {
-            $this->getOwner()->RegenerateTokens = false;
+        if ($owner->RegenerateTokens) {
+            $owner->RegenerateTokens = false;
             $this->generateTokens();
         }
     }
@@ -34,12 +37,14 @@ class TokenAccessible extends DataExtension
     {
         parent::updateCMSFields($fields);
 
+        /** @var \SilverStripe\ORM\DataObject $owner */
+        $owner = $this->getOwner();
         $token = $this->userToken();
 
         if (!$token) {
             $token = "This user token can no longer be displayed - if you do not know this value, regenerate tokens by selecting Regenerate below";
         } else {
-            $token = $this->getOwner()->ID . ':' . $token;
+            $token = $owner->ID . ':' . $token;
         }
 
         $readOnly = ReadonlyField::create('DisplayToken', 'Token', $token);
@@ -54,8 +59,14 @@ class TokenAccessible extends DataExtension
     public function onAfterWrite()
     {
         if ($this->authToken) {
+            /** @var \SilverStripe\ORM\DataObject $owner */
+            $owner = $this->getOwner();
+
             // store the new token so it can be displayed later
-            Controller::curr()->getRequest()->getSession()->set('member_auth_token_' . $this->getOwner()->ID, $this->authToken);
+            $controller = Controller::curr();
+            if($controller instanceof Controller) {
+                $controller->getRequest()->getSession()->set('member_auth_token_' . $owner->ID, $this->authToken);
+            }
         }
     }
 
@@ -68,15 +79,18 @@ class TokenAccessible extends DataExtension
     {
         $generator = new RandomGenerator();
         $token = $generator->randomToken('sha1');
-        $member = $this->getOwner();
-        if($member instanceof Member) {
-            $this->getOwner()->Token = $member->encryptWithUserSettings($token);
+        $owner = $this->getOwner();
+        if($owner instanceof Member) {
+            $owner->Token = $owner->encryptWithUserSettings($token);
             $this->authToken = $token;
         }
     }
 
     public function userToken()
     {
-        return Controller::has_curr() ? Controller::curr()->getRequest()->getSession()->get('member_auth_token_' . $this->getOwner()->ID) : null;
+        /** @var \SilverStripe\ORM\DataObject $owner */
+        $owner = $this->getOwner();
+        $controller = Controller::curr();
+        return $controller ? $controller->getRequest()->getSession()->get('member_auth_token_' . $owner->ID) : null;
     }
 }
